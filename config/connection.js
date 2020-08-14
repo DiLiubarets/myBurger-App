2f244dc61313286404355f95625a121918b297ea
Dina <diana.liubarets@hotmail.com>
2020-08-14 18:06:30 -0400

    heroku deploy

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,21 @@
 const mysql = require("mysql");
 const dbConfig = require("../db.config")
 
-var con = mysql.createConnection({
+var con = mysql.createPool({
   host: dbConfig.HOST,
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
   database: dbConfig.DB,
   multipleStatements: true,
 });
-con.connect((err) => {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + con.threadId);
-});
+
+// con.acquireConnection
+
+// con.connect((err) => {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + con.threadId);
+// });
 module.exports = con;
